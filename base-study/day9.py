dcef67789f8eb45b4820ab5a2fe016fb56856b11
johnny <hyyh0369@163.com>
2020-04-02 21:06:34 +0800

    day9.py

---
+++ base-study/day9.py
@@ -0,0 +1,27 @@
+# 列表排序
+# list1 = ['orange', 'apple', 'zoo', 'internationalization', 'blueberry']
+# list2 = sorted(list1)
+# # sort函数返回列表排序后的拷贝不会修改传入的列表
+# list3 = sorted(list1, reverse=True)
+# # 通过key关键字参数指定根据字符串长度进行排序而不是默认的字母表顺序
+# list4 = sorted(list1, key=len)
+# print(list1)
+# print(list2)
+# print(list3)
+# print(list4)
+# # 给列表对象发出排序消息直接在列表对象上进行排序
+# list1.sort(reverse=True)
+# print(list1)
+
+# 列表切片
+# fruits = ['grape', 'apple', 'strawberry', 'waxberry']
+# fruits += ['pitaya', 'pear', 'mango']
+# # 列表图片
+# fruits2 = fruits[1:4]
+# print(fruits2)
+# fruits3 = fruits[:]
+# print(fruits3)
+# fruits4 = fruits[-3:-1]
+# print(fruits4)
+# fruits5 = fruits[::-1]
+# print(fruits5)
